ebbc308f89c2c2f50fd4e5b06e3c49a476bc46e5
Brian Wiec <bswiec@gmail.com>
2023-04-03 15:44:10 -0400

    Fixed mismatched signal dimensions

--- rtl/ccm.vhd
+++ rtl/ccm.vhd
@@ -8,15 +8,15 @@ entity ccm is
 		clk : in std_logic;
 		rst : in std_logic;
 		
-		c00 : in std_logic_vector (7 downto 0);
-		c01 : in std_logic_vector (7 downto 0);
-		c02 : in std_logic_vector (7 downto 0);
-		c10 : in std_logic_vector (7 downto 0);
-		c11 : in std_logic_vector (7 downto 0);
-		c12 : in std_logic_vector (7 downto 0);
-		c20 : in std_logic_vector (7 downto 0);
-		c21 : in std_logic_vector (7 downto 0);
-		c22 : in std_logic_vector (7 downto 0);
+		c00 : in std_logic_vector (15 downto 0);
+		c01 : in std_logic_vector (15 downto 0);
+		c02 : in std_logic_vector (15 downto 0);
+		c10 : in std_logic_vector (15 downto 0);
+		c11 : in std_logic_vector (15 downto 0);
+		c12 : in std_logic_vector (15 downto 0);
+		c20 : in std_logic_vector (15 downto 0);
+		c21 : in std_logic_vector (15 downto 0);
+		c22 : in std_logic_vector (15 downto 0);
 		
 		hblank_in : in std_logic;
 		vblank_in : in std_logic;
@@ -29,27 +29,27 @@ entity ccm is
 end ccm;
 
 architecture behavioral of ccm is
-	signal r_in : std_logic_vector (7 downto 0) := (others => '0');
-	signal g_in : std_logic_vector (7 downto 0) := (others => '0');
-	signal b_in : std_logic_vector (7 downto 0) := (others => '0');
-	signal m00 : signed (15 downto 0) := (others => '0');
-	signal m01 : signed (15 downto 0) := (others => '0');
-	signal m02 : signed (15 downto 0) := (others => '0');
-	signal m02_1 : signed (15 downto 0) := (others => '0');
-	signal m10 : signed (15 downto 0) := (others => '0');
-	signal m11 : signed (15 downto 0) := (others => '0');
-	signal m12 : signed (15 downto 0) := (others => '0');
-	signal m12_1 : signed (15 downto 0) := (others => '0');
-	signal m20 : signed (15 downto 0) := (others => '0');
-	signal m21 : signed (15 downto 0) := (others => '0');
-	signal m22 : signed (15 downto 0) := (others => '0');
-	signal m22_1 : signed (15 downto 0) := (others => '0');
-	signal adder_r_1 : signed (15 downto 0) := (others => '0');
-	signal adder_r_2 : signed (15 downto 0) := (others => '0');
-	signal adder_g_1 : signed (15 downto 0) := (others => '0');
-	signal adder_g_2 : signed (15 downto 0) := (others => '0');
-	signal adder_b_1 : signed (15 downto 0) := (others => '0');
-	signal adder_b_2 : signed (15 downto 0) := (others => '0');
+	signal r_in : std_logic_vector (15 downto 0) := (others => '0');
+	signal g_in : std_logic_vector (15 downto 0) := (others => '0');
+	signal b_in : std_logic_vector (15 downto 0) := (others => '0');
+	signal m00 : signed (31 downto 0) := (others => '0');
+	signal m01 : signed (31 downto 0) := (others => '0');
+	signal m02 : signed (31 downto 0) := (others => '0');
+	signal m02_1 : signed (31 downto 0) := (others => '0');
+	signal m10 : signed (31 downto 0) := (others => '0');
+	signal m11 : signed (31 downto 0) := (others => '0');
+	signal m12 : signed (31 downto 0) := (others => '0');
+	signal m12_1 : signed (31 downto 0) := (others => '0');
+	signal m20 : signed (31 downto 0) := (others => '0');
+	signal m21 : signed (31 downto 0) := (others => '0');
+	signal m22 : signed (31 downto 0) := (others => '0');
+	signal m22_1 : signed (31 downto 0) := (others => '0');
+	signal adder_r_1 : signed (31 downto 0) := (others => '0');
+	signal adder_r_2 : signed (31 downto 0) := (others => '0');
+	signal adder_g_1 : signed (31 downto 0) := (others => '0');
+	signal adder_g_2 : signed (31 downto 0) := (others => '0');
+	signal adder_b_1 : signed (31 downto 0) := (others => '0');
+	signal adder_b_2 : signed (31 downto 0) := (others => '0');
 	signal adder_r_2_scaled : signed (7 downto 0) := (others => '0');
 	signal adder_g_2_scaled : signed (7 downto 0) := (others => '0');
 	signal adder_b_2_scaled : signed (7 downto 0) := (others => '0');
@@ -89,9 +89,9 @@ begin
 				adder_b_2 <= (others => '0');
 			else
 				-- Input registers
-				r_in <= din(7 downto 0);
-				g_in <= din(15 downto 8);
-				b_in <= din(23 downto 16);
+				r_in <= din(7 downto 0) & X"00";
+				g_in <= din(15 downto 8) & X"00";
+				b_in <= din(23 downto 16) & X"00";
 				
 				-- Coefficient multiplication
 				m00 <= signed(c00) * signed(r_in);
@@ -121,12 +121,12 @@ begin
 	end process;
 	
 	-- Scale back to 8 bits per component
-	adder_r_2_scaled <= adder_r_2(15 downto 8);
-	adder_g_2_scaled <= adder_g_2(15 downto 8);
-	adder_b_2_scaled <= adder_b_2(15 downto 8);
+	adder_r_2_scaled <= adder_r_2(adder_r_2'left downto adder_r_2'left-7);
+	adder_g_2_scaled <= adder_g_2(adder_g_2'left downto adder_g_2'left-7);
+	adder_b_2_scaled <= adder_b_2(adder_b_2'left downto adder_b_2'left-7);
 
 	-- Assign output wire
-	dout <= std_logic_vector(adder_b_2) & std_logic_vector(adder_g_2) & std_logic_vector(adder_r_2);
+	dout <= std_logic_vector(adder_b_2_scaled) & std_logic_vector(adder_g_2_scaled) & std_logic_vector(adder_r_2_scaled);
 
 	-- Match blanking signal delays with datapath delays
 	process (clk) begin
